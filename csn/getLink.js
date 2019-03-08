aa1bd4fc0087306cb7c38519ae994b554d80e477
lana <sitdownrightnow2552@gmail.com>
2019-03-08 22:04:07 +0700

    v1.1

--- csn/getLink.js
+++ csn/getLink.js
@@ -1,20 +1,25 @@
 var CSNSrcLink = {};
-getCSNSrcLinkAndSendToContent();
+getCSNSrcLinks();
+document.dispatchEvent(new CustomEvent('gotLinkFromCSN', {detail: CSNSrcLink}));
 
-function getCSNSrcLinkAndSendToContent() {
-    const types = ['128', '320', 'flac'];
-    const sources = player.getConfig().sources;
+function getCSNSrcLinks() {
     let links = {};
-    for (let source of sources) {
-        for (let type of types) {
-            if (source.file.match(regexFrom(type))) links[type] = source.file
+    const types = ['128', '320', 'flac'];
+    try {
+        const sources = player.getConfig().sources;
+        for (let source of sources) {
+            for (let type of types) {
+                if (source.file.match(regexFrom(type))) links[type] = source.file
+            }
         }
+        if (links['320']) {
+            if (links['128']) delete links['128']
+        }
+    } catch (e ) {
+        //ignore , when this happened means the player is not defined, this is not player page
+    } finally {
+        CSNSrcLink = links;
     }
-    if (links['320']) {
-        if (links['128']) delete links['128']
-    }
-    document.dispatchEvent(new CustomEvent('gotLinkFromCSN', {detail: links}));
-    CSNSrcLink = links;
 }
 
 function regexFrom(string) {
