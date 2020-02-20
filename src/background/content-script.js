4cab47e0318c7651b18b4a470684a532837aa5ca
Lana <sitdownrightnow2552@gmail.com>
2020-02-21 01:38:11 +0700

    refactor
    use single quote

--- src/background/content-script.js
+++ src/background/content-script.js
@@ -1,12 +1,12 @@
 //inject script into page
-const startupScript = document.createElement("script");
-startupScript.src = chrome.runtime.getURL("src/csn-embedded.js");
+const startupScript = document.createElement('script');
+startupScript.src = chrome.runtime.getURL('src/csn-embedded.js');
 document.body.appendChild(startupScript);
 
-document.addEventListener("gotLinkFromCSN", (event) => {
+document.addEventListener('gotLinkFromCSN', (event) => {
     chrome.runtime.sendMessage(event.detail);
 });
 
 chrome.runtime.onMessage.addListener(config => {
-    document.dispatchEvent(new CustomEvent("startDownload", {detail: config}))
+    document.dispatchEvent(new CustomEvent('startDownload', { detail: config }));
 });
