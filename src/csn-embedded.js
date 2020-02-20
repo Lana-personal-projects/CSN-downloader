9d9d1aa113bad6a18d69c73a7c03b0e7be452abe
Lana <sitdownrightnow2552@gmail.com>
2020-02-21 01:30:01 +0700

    find flac links
    now the flac link is not included in frontend, so we have to generate it from existed links

--- src/csn-embedded.js
+++ src/csn-embedded.js
@@ -1,23 +1,26 @@
-// inject this script into page to access pages"s js object (player) and communicate
+// inject this script into page to access pages's js object (player) and communicate
 // with background via content-script by an event
-// put this into function to avoid conflict with page"s js
-(() => {
+// put this into function to avoid conflict with page's js
+(async () => {
     const links = {};
-    const qualities = ["128", "320", "flac", "m4a"];
+    const qualities = ['128', '320', 'flac', 'm4a'];
     try {
-        // player is page"s js object that control the player
+        // player is page's js object that control the player
         for (const source of player.getConfig().sources) {
             for (const quality of qualities) {
                 if (source.file.match(regexFor(quality)))
                     links [quality] = source.file;
             }
         }
+        if (!links['flac']) {
+            await fixFlacLink(links);
+        }
     } catch (e) {
         // player is not defined, probably this is not a player page.
     }
 
-    document.dispatchEvent(new CustomEvent("gotLinkFromCSN", {detail: links}));
-    document.addEventListener("startDownload", event => {
+    document.dispatchEvent(new CustomEvent('gotLinkFromCSN', { detail: links }));
+    document.addEventListener('startDownload', event => {
         const config = event.detail;
         const availableQualities = Object.keys(links);
         const download = availableQualities.filter(quality => config.qualities.includes(quality));
@@ -29,13 +32,11 @@
         function downloadNext(i) {
             if (i >= downloadQualities.length) return;
 
-            const a = document.createElement("A");
+            const a = document.createElement('A');
             a.href = links[downloadQualities[i]];
-            a.target = "_parent";
-            a.setAttribute("download", "download");
-            (document.body || document.documentElement).appendChild(a);
+            a.target = '_parent';
+            a.setAttribute('download', 'download');
             a.click();
-            a.parentNode.removeChild(a);
             setTimeout(() => downloadNext(i + 1), 800);
         }
 
@@ -43,6 +44,52 @@
     }
 
     function regexFor(quality) {
-        return new RegExp(`\\/downloads\\/.+\\/${quality}\\/`, "g");
+        return new RegExp(`\\/downloads\\/.+\\/${quality}\\/`, 'g');
+    }
+
+    async function fixFlacLink(links) {
+        if (links.hasOwnProperty('m4a')) {
+            const flacLink = links['m4a'].replace(/(?<=[/.])m4a/g, 'flac');
+            if (await testLink(flacLink)) {
+                links['flac'] = flacLink;
+                return;
+            }
+        }
+
+        for (const quality of ['320', '128']) {
+            if (links.hasOwnProperty(quality)) {
+                const flacLink = links[quality]
+                    .replace(new RegExp(`(?<=/)${quality}`, 'g'), 'flac')
+                    .replace(/(?<=\.)mp3/g, 'flac');
+                if (await testLink(flacLink)) {
+                    links['flac'] = flacLink;
+                    return;
+                }
+            }
+        }
+
+    }
+
+    // create a audio element to check if link ok
+    // by this way we can bypass the cors
+    async function testLink(link) {
+        const audio = document.createElement('AUDIO');
+        try {
+            await new Promise((resolve, reject) => {
+                audio.addEventListener('loadedmetadata', () => {
+                    resolve();
+                });
+                audio.addEventListener('error', () => {
+                    reject();
+                });
+                audio.src = link;
+                audio.load();
+            });
+
+            console.log(`found hidden link: ${link}`);
+            return true;
+        } catch (e) {
+            return false;
+        }
     }
 })();
