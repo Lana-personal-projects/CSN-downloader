a4672136637d8214dbfd3e159a8967dfc2b0e3c4
Lana <sitdownrightnow2552@gmail.com>
2020-02-21 01:37:07 +0700

    fix config undefined on first start

--- src/background/background.js
+++ src/background/background.js
@@ -1,12 +1,13 @@
-chrome.storage.sync.get(["qualities", "fallbacks"], async config => {
+chrome.storage.sync.get(['qualities', 'fallbacks'], async config => {
     //set default config
     if (!config.qualities || !config.fallbacks) {
+        config = {
+            qualities: ['320'],
+            fallbacks: ['128'],
+        };
         await new Promise(resolve => {
-            chrome.storage.sync.set({
-                qualities: ["320"],
-                fallbacks: ["128"]
-            }, () => resolve());
-        })
+            chrome.storage.sync.set(config, () => resolve());
+        });
     }
 
     chrome.browserAction.onClicked.addListener(tab => {
@@ -15,16 +16,16 @@ chrome.storage.sync.get(["qualities", "fallbacks"], async config => {
 
     chrome.runtime.onMessage.addListener(links => {
         const keys = Object.keys(links);
-        const available = keys.join(" ");
-        const download = keys.filter(key => config.qualities.includes(key)).join(" ");
-        const fallbackDownload = keys.filter(key => config.fallbacks.includes(key)).join(" ");
+        const available = keys.join(' ');
+        const download = keys.filter(key => config.qualities.includes(key)).join(' ');
+        const fallbackDownload = keys.filter(key => config.fallbacks.includes(key)).join(' ');
         chrome.browserAction.setTitle({
-            title: `Download: ${download || fallbackDownload || "None"}\nAvailable: ${available || "None"}`
+            title: `Download: ${download || fallbackDownload || 'None'}\nAvailable: ${available || 'None'}`,
         });
     });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if (changeInfo.status === "loading")
-        chrome.browserAction.setTitle({title: "Getting sources..."});
+    if (changeInfo.status === 'loading')
+        chrome.browserAction.setTitle({ title: 'Getting sources...' });
 });
