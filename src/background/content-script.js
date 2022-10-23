6c820a74a7432e7a1ae309d779d8dbc917f87a7a
Toannd <sitdownrightnow.dev@gmail.com>
2022-10-23 16:33:42 +0700

    perf: compress icon image, defer script loading

--- src/background/content-script.js
+++ src/background/content-script.js
@@ -1,10 +1,11 @@
 //inject script into page
 const startupScript = document.createElement('script');
 startupScript.src = chrome.runtime.getURL('src/csn-embedded.js');
+startupScript.defer = true;
 document.body.appendChild(startupScript);
 
 document.addEventListener('gotLinkFromCSN', (event) => {
-    chrome.runtime.sendMessage(event.detail);
+    chrome.runtime.sendMessage(event.detail).catch(e => console.error(e));
 });
 
 chrome.runtime.onMessage.addListener(config => {
