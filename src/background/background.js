a78b163ee77bf4f016c4936e68cd15d66bf8c99b
Toannd <sitdownrightnow.dev@gmail.com>
2022-10-23 16:12:28 +0700

    feat: update to chrome extension manifest v3

--- src/background/background.js
+++ src/background/background.js
@@ -1,36 +1,39 @@
-chrome.storage.sync.get(['qualities', 'fallbacks'], async config => {
+let config = {
+    qualities: ['128', '320', 'flac'],
+    fallbacks: [],
+};
+
+chrome.storage.sync.get(['qualities', 'fallbacks'], async (conf) => {
     //set default config
-    if (!config.qualities || !config.fallbacks) {
-        config = {
-            qualities: ['320'],
-            fallbacks: ['128'],
-        };
+    if (!conf.qualities || !conf.fallbacks) {
+        conf = { ...config, ...conf };
         await new Promise(resolve => {
-            chrome.storage.sync.set(config, () => resolve());
+            chrome.storage.sync.set(conf, () => resolve());
         });
+        return;
     }
+    config = conf;
+});
 
-    chrome.browserAction.onClicked.addListener(tab => {
-        chrome.tabs.sendMessage(tab.id, config);
-    });
+chrome.action.onClicked.addListener(tab => {
+    chrome.tabs.sendMessage(tab.id, config);
+});
 
-    chrome.runtime.onMessage.addListener(links => {
-        const keys = Object.keys(links);
-        const available = keys.join(' ');
-        const download = keys.filter(key => config.qualities.includes(key)).join(' ');
-        const fallbackDownload = keys.filter(key => config.fallbacks.includes(key)).join(' ');
-        chrome.browserAction.setTitle({
-            title: `Download: ${download || fallbackDownload || 'None'}\nAvailable: ${available || 'None'}`,
-        });
-    });
-
-    chrome.storage.onChanged.addListener((changes) => {
-        if (changes.qualities) config.qualities = changes.qualities.newValue;
-        if (changes.fallbacks) config.fallbacks = changes.fallbacks.newValue;
+chrome.runtime.onMessage.addListener(links => {
+    const keys = Object.keys(links);
+    const available = keys.join(' ');
+    const download = keys.filter(key => config.qualities.includes(key)).join(' ');
+    const fallbackDownload = keys.filter(key => config.fallbacks.includes(key)).join(' ');
+    chrome.action.setTitle({
+        title: `Download: ${download || fallbackDownload || 'None'}\nAvailable: ${available || 'None'}`,
     });
 });
 
+chrome.storage.onChanged.addListener((changes) => {
+    if (changes.qualities) config.qualities = changes.qualities.newValue;
+    if (changes.fallbacks) config.fallbacks = changes.fallbacks.newValue;
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if (changeInfo.status === 'loading')
-        chrome.browserAction.setTitle({ title: 'Getting sources...' });
+    if (changeInfo.status === 'loading') chrome.action.setTitle({ title: 'Getting sources...' });
 });
