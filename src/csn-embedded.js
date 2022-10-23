9a1d7610585fbf228e6acfd561374589cdcedbe6
Toannd <sitdownrightnow.dev@gmail.com>
2022-10-23 16:31:37 +0700

    feat: update link matching

--- src/csn-embedded.js
+++ src/csn-embedded.js
@@ -1,28 +1,94 @@
-// inject this script into page to access pages's js object (player) and communicate
+// inject this script into page to access page's js object (player) and communicate
 // with background via content-script by an event
 // put this into function to avoid conflict with page's js
 (async () => {
-    const links = {};
+    // player is page's js object that control the player
+    const player = window.player;
+    if (!player) {
+        console.log('Player not found');
+        return;
+    }
+
     const qualities = ['128', '320', 'flac'];
-    try {
-        // player is page's js object that control the player
-        if (!player) {
-            console.log('Player not found');
-            return;
-        }
+    const links = findPublicLinks();
+    await tryFindHiddenLinks(links).catch(e => console.error('Error hidden flac hidden links', e));
+    await tryFindFlacHiddenLink(links).catch(e => console.error('Error find flac hidden links', e));
+
+    function findPublicLinks() {
+        const links = {};
         for (const source of player.getConfig().sources) {
             for (const quality of qualities) {
-                if (source.file.match(regexForDownloadLink(quality)))
+                if (matchQuality(quality, source.file))
                     links [quality] = source.file;
             }
         }
-
-        await fixMp3Links(links);
-        await fixFlacLink(links);
-    } catch (e) {
-        console.log(e);
+        return links;
     }
 
+    function matchQuality(quality, link) {
+        const storages = ['down2', 'download', 'downloads'];
+        const regexes = storages.map(storage => new RegExp(`\\/${storage}.+\\/${quality}\\/`, 'g'));
+        return regexes.some(reg => link.match(reg));
+    }
+
+    async function tryFindFlacHiddenLink(links) {
+        if (links['flac']) {
+            return;
+        }
+        const replaceable = qualities.filter(quality => quality !== 'flac');
+        for (const quality of replaceable) {
+            if (links.hasOwnProperty(quality)) {
+                const flacLink = links[quality]
+                    .replace(new RegExp(`(?<=/)${quality}`, 'g'), 'flac')
+                    .replace(/(?<=\.)mp3/g, 'flac');
+                if (await testLink(flacLink)) {
+                    links['flac'] = flacLink;
+                    return;
+                }
+            }
+        }
+    }
+
+    async function tryFindHiddenLinks(links) {
+        if (!links['128']) {
+            return;
+        }
+        const fixable = qualities.filter(quality => quality !== '128' || quality !== 'flac');
+        for (const quantity of fixable) {
+            if (links.hasOwnProperty(quantity)) {
+                continue;
+            }
+            const mp3Link = links['128'].replace(new RegExp('(?<=/)128', 'g'), quantity);
+            if (await testLink(mp3Link)) {
+                links[quantity] = mp3Link;
+            }
+        }
+    }
+
+    // create an audio element to check if link ok
+    // by this way we can bypass the cors
+    async function testLink(link) {
+        const audio = document.createElement('AUDIO');
+        try {
+            await new Promise((resolve, reject) => {
+                audio.addEventListener('loadedmetadata', () => {
+                    resolve();
+                });
+                audio.addEventListener('error', () => {
+                    reject();
+                });
+                audio.src = link;
+                audio.load();
+            });
+
+            console.log(`Found hidden link: ${link}`);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    console.log(links);
     document.dispatchEvent(new CustomEvent('gotLinkFromCSN', { detail: links }));
     document.addEventListener('startDownload', event => {
         const config = event.detail;
@@ -45,66 +111,5 @@
         }
 
         downloadNext(0);
-    }
-
-    function regexForDownloadLink(quality) {
-        return new RegExp(`\\/download.+\\/${quality}\\/`, 'g');
-    }
-
-    async function fixFlacLink(links) {
-        if (links['flac']) {
-            return;
-        }
-        const replaceable = qualities.filter(quality => quality !== 'flac');
-        for (const quality of replaceable) {
-            if (links.hasOwnProperty(quality)) {
-                const flacLink = links[quality]
-                    .replace(new RegExp(`(?<=/)${quality}`, 'g'), 'flac')
-                    .replace(/(?<=\.)mp3/g, 'flac');
-                if (await testLink(flacLink)) {
-                    links['flac'] = flacLink;
-                    return;
-                }
-            }
-        }
-    }
-
-    async function fixMp3Links(links) {
-        if (!links['128']) {
-            return;
-        }
-        const fixable = qualities.filter(quality => quality !== '128' || quality !== 'flac');
-        for (const quantity of fixable) {
-            if (links.hasOwnProperty(quantity)) {
-                continue;
-            }
-            const mp3Link = links['128'].replace(new RegExp('(?<=/)128', 'g'), quantity);
-            if (await testLink(mp3Link)) {
-                links[quantity] = mp3Link;
-            }
-        }
-    }
-
-    // create a audio element to check if link ok
-    // by this way we can bypass the cors
-    async function testLink(link) {
-        const audio = document.createElement('AUDIO');
-        try {
-            await new Promise((resolve, reject) => {
-                audio.addEventListener('loadedmetadata', () => {
-                    resolve();
-                });
-                audio.addEventListener('error', () => {
-                    reject();
-                });
-                audio.src = link;
-                audio.load();
-            });
-
-            console.log(`Found hidden link: ${link}`);
-            return true;
-        } catch (e) {
-            return false;
-        }
     }
 })();
